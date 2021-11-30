54ed12339cc7aa49bd0a1031ebab260180a95aaa
yangjiaqi08 <yangjiaqi08@meituan.com>
2021-11-30 17:31:53 +0800

    feat:　init

---
+++ wingman/wingmanApp.swift
@@ -0,0 +1,17 @@
+//
+//  wingmanApp.swift
+//  wingman
+//
+//  Created by yangjiaqi08 on 2021/11/30.
+//
+
+import SwiftUI
+
+@main
+struct wingmanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
